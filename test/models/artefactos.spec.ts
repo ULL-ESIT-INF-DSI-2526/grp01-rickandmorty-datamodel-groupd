68308a733dde341297b42788341a2fdda691bdcc
Pablo Perez Hernandez <alu0101619002@ull.edu.es>
2026-03-21 13:52:50 +0000

    feat: extended Artefacto tests

--- test/models/artefactos.spec.ts
+++ test/models/artefactos.spec.ts
@@ -41,7 +41,7 @@ describe('Test para comprobar el funcionamiento de la clase Artefacto', () => {
     '',
   );
 
-  test('Se puede instanciar un objeto sin problemas', () => {
+  describe('Se puede instanciar un objeto sin problemas', () => {
     const art1 = new Artefacto(
       'I-345',
       'Megasiembra',
@@ -50,6 +50,24 @@ describe('Test para comprobar el funcionamiento de la clase Artefacto', () => {
       new Nivel(6.7),
       'Ayuda a los agicultores galacticos',
     );
+    test('Id', () => {
+      expect(art1.id).toEqual('I-345');
+    });
+    test('Nombre', () => {
+      expect(art1.nombre).toEqual('Megasiembra');
+    });
+    test('Inventor', () => {
+      expect(art1.inventor).toEqual(personaje1);
+    });
+    test('Tipo', () => {
+      expect(art1.tipo).toEqual('Biotecnología');
+    });
+    test('Nivel de peligrosidad', () => {
+      expect(art1.nivel_peligrosidad.nivel).toEqual(6.7);
+    });
+    test('Descripción', () => {
+      expect(art1.descripcion).toEqual('Ayuda a los agicultores galacticos');
+    });
 
     expect(art1.info()).toEqual(
       '[I-345] Megasiembra fue inventado por Morty.\nEs de tipo Biotecnología y tiene un nivel de peligrosidad de 6.7.\nMás información: Ayuda a los agicultores galacticos',
