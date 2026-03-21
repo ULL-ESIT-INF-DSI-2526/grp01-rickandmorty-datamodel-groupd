d4b9bbffecb871160faf0eb4aa3a0a8b47ad4cfb
Pablo Perez Hernandez <alu0101619002@ull.edu.es>
2026-03-21 13:50:42 +0000

    feat: extended Especie tests

--- test/models/especies.spec.ts
+++ test/models/especies.spec.ts
@@ -12,7 +12,7 @@ describe('Tests para comprobar el funcionamiento de la clase Especie', () => {
     new Nivel(9.4),
     'Especies',
   );
-  test('Se puede crear un objeto especie sin errores', () => {
+  describe('Se puede crear un objeto especie sin errores', () => {
     const esp1 = new Especies(
       'E-14',
       'Ratadragones',
@@ -21,6 +21,24 @@ describe('Tests para comprobar el funcionamiento de la clase Especie', () => {
       TipoEspecies.Amorfo,
       3,
     );
+    test('Id', () => {
+      expect(esp1.id).toEqual('E-14');
+    });
+    test('Nombre', () => {
+      expect(esp1.nombre).toEqual('Ratadragones');
+    });
+    test('Descripción', () => {
+      expect(esp1.descripcion).toEqual('Lanza fuego');
+    });
+    test('Dimensión de origen', () => {
+      expect(esp1.origen).toEqual(dim1);
+    });
+    test('Tipo', () => {
+      expect(esp1.tipo).toEqual('Amorfo');
+    });
+    test('Esperanza de vida media', () => {
+      expect(esp1.esperanza_vida).toEqual(3);
+    });
     expect(esp1.info()).toEqual(
       '[E-14] Ratadragones es originaria de Cronenberg es de tipo Amorfo.\nTiene una esperanza de vida media de 3 años humanos.\nMás información: Lanza fuego',
     );
